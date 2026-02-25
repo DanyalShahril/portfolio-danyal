4c02cf86bcd9bb60123b6098412e0a0e17e91a8d
DanyalShahril <66882479+DanyalShahril@users.noreply.github.com>
2026-02-25 08:27:46 +0800

    Revise footer to use span for dynamic year
    Updated footer to dynamically display the current year.

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,1 @@
-<footer class="footer">
-    <div class="container">
-        <p>© <?php echo date("Y"); ?> Danyal Shahril | Selangor, Malaysia</p>
-        <button onclick="scrollToTop()" class="back-top">↑</button>
-    </div>
-</footer>
-
-<script src="assets/js/main.js"></script>
-</body>
-</html>
+<p>© <span id="year"></span> Danyal Shahril | Selangor, Malaysia</p>
